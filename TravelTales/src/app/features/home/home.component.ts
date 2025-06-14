b8b0ca3c84036f10fb14beb75a200328b02203b7
George-Cristian Pîrvu <george.pirvuu@gmail.com>
2025-06-14 19:08:11 +0300

    Added Content to Home Page

--- TravelTales/src/app/features/home/home.component.ts
+++ TravelTales/src/app/features/home/home.component.ts
@@ -5,17 +5,7 @@ import { CommonModule } from '@angular/common';
   selector: 'app-home',
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <div class="home-container">
-      <h1>Welcome to Travel Tales</h1>
-      <p>Your journey starts here!</p>
-    </div>
-  `,
-  styles: [`
-    .home-container {
-      padding: 2rem;
-      text-align: center;
-    }
-  `]
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
 })
 export class HomeComponent {}
